169990ecb6266b314a4ef3fa2d2e06513632efb7
ankitasingh20081998 <109068867+ankitasingh20081998@users.noreply.github.com>
2022-07-11 15:24:03 +0530

    if condition examples

---
+++ ifcond.py
@@ -0,0 +1,102 @@
+#  1.........Max Size from 2: WAP to input two spp size and print the max spp size.
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# if a > b:
+#     print(f"First number: {a} is max size than Second number: {b}")
+# elif b > a:
+#     print(f"Second number: {b} is max size than First number: {a}") 
+# else:   s
+#     print(f"First number: {a} is equal to Second number: {b}")  
+
+
+
+
+# 2......... Greetings: WAP to build predefined users dictionary, request key input from user, if key exist in dictionary print the respective values.
+# users = {‘u1’:‘Administrator’, ‘u2’:‘ServerAdmin’, ‘u3’:‘NetworkAdmin’}
+
+# users = {'u1':'Administrator', 'u2':'ServerAdmin', 'u3':'NetworkAdmin'}
+# key = input("Enter a key: ")
+# if key in users:
+#     print(f"key exist in dictionary {users[key]}")
+# else:
+#     print(f"key does not exist in dictionary {key}")    
+
+
+
+
+# 3..........Max Size from 3: WAP to input three spp size and print the max spp size.
+
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: "))
+# c = float(input("Enter third number: "))
+# if a > b and a > c:
+#     print(f"First number: {a} is max size")
+# elif b > a and b > c:
+#     print(f"First number: {b} is max size")
+# else:
+#     if a == b and a == c:
+#         print("All number are equal")
+#     print(f"Third number: {c} is max size")    
+
+
+
+
+# 4..........WAP to check given number is odd or even.
+
+# a = int(input("Enter first number: "))
+# if a % 2 == 0:
+#     print(f"Entered  number: {a} is even ")
+# else:
+#     print(f"entered number: {a} is odd")
+
+
+
+
+# 5..........Check User name :WAP to check user name exist in given list of users.
+
+# name = ["Ankita","Alka","Anjali","Nidhi","Chitralekha","Shalini","Rajiv"]
+# print(name)
+
+# print(type(name))
+# print(name[0])
+
+# for i in name:
+#     if i == "Ankita":
+#         print("Ankita is exist in name")
+
+# if "Alka" in name:
+#     print("Alka is exist in name")
+# else:
+#     print("not exist")
+   
+# print("Anjali" in name)
+# print("Anita" in name)
+
+
+# 6..........Check User name :WAP to check user name exist in given tuple of users. 
+
+# tuplex = (3,1,"s","h","q",)
+# print(3 in tuplex)
+# print("o" in tuplex)
+
+# print(tuplex[2])
+# print(len(tuplex))
+# print(sorted(tuplex))
+
+
+
+# 7..........Check User name :WAP to check user name exist in given dictionary of users.
+
+# dict = {
+#     "a":"apple",
+#      "b":"ball",
+#      "d":"dog",
+#      "c":"cat"
+# }
+# print(dict['c'])
+# print(sorted(dict))
+# print(len(dict))
+# print(dict.keys())
+
+# print("a" in dict)
